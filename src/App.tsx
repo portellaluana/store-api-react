40e16d09894d74569da7cc065a9707815f01dda5
portellaluana <luanaportella@gmail.com>
2024-03-21 21:19:08 -0300

    fix/wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,6 @@ import { Provider } from "./context/Provider";
 import Details from "./pages/product-details/Details";
 import {APIError} from "./pages/APIError/APIError";
 import { PageProductNotFound } from "./pages/productNotFound/productNotFound";
-import { Eletronicos } from "./pages/categorias/Eletronicos";
-import { Joalheria } from "./pages/categorias/Joalheria";
-import { Homens } from "./pages/categorias/Homens";
-import { Mulheres } from "./pages/categorias/Mulheres";
 import { PaginaCategoria } from "./pages/categorias/PaginaCategoria";
 
 function App() {
@@ -25,11 +21,6 @@ function App() {
           <Route path="/details" element={<Details />} />
           <Route path="/produto-não-encontrado" element={<PageProductNotFound />} />
           <Route path="/categoria/:categoria" element={<PaginaCategoria />}/>
-          {/* <Route path="/homens" element={<Homens />} />
-          <Route path="/mulheres" element={<Mulheres />} />
-          <Route path="/joalheria" element={<Joalheria />} /> */}
-
-
         </Routes>
       </Router>
     </Provider>
